5344e556f6f8151c0ef4d068679d71061646e557
Stephen Michaelis <Stephen.Michaelis@hotmail.com>
2020-02-20 06:29:08 -0500

    converts relative paths to absolute

--- tmx/tmx.go
+++ tmx/tmx.go
@@ -49,15 +49,15 @@ func LoadTMX(source string) (*TMX, error) {
 
 	// fmt.Println(tmxPath)
 
-	absTMXPath, err := filepath.Abs(tmxPath)
-	if err != nil {
-		return nil, err
-	}
+	// absTMXPath, err := filepath.Abs(tmxPath)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	fmt.Println("tmx:", absTMXPath)
+	// fmt.Println("tmx:", absTMXPath)
 
 	// Unmarshal the tmx path.
-	tmxBytes, err := ioutil.ReadFile(absTMXPath)
+	tmxBytes, err := ioutil.ReadFile(tmxPath)
 	if err != nil {
 		return nil, fmt.Errorf("error reading tmx file: %w", err)
 	}
